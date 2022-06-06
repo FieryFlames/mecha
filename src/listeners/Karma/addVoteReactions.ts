cae909a69b24d258c5b04661cbc3edaead3b6a13
Jack Matthews <jm5112356@gmail.com>
2022-06-06 00:53:40 -0400

    Automatically react to messages in karma channel

---
+++ src/listeners/Karma/addVoteReactions.ts
@@ -0,0 +1,19 @@
+import { Listener } from '@sapphire/framework';
+import type { Message } from 'discord.js';
+
+export class addVoteReactionsListener extends Listener {
+	public constructor(context: Listener.Context, options: Listener.Options) {
+		super(context, {
+			...options,
+			event: 'messageCreate'
+		});
+	}
+
+	public async run(message: Message) {
+		if (message.author.bot) return
+		else if (message.channel.id != process.env.KARMA_CHANNEL_ID) return
+
+		await message.react('⬆️')
+		await message.react('⬇️')
+	}
+}
