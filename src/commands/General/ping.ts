270d65eaf0a9534d4ecd330809f0b2a55cbb450f
Jack Matthews <jm5112356@gmail.com>
2022-06-05 23:46:09 -0400

    Convert ping into a slash command

--- src/commands/General/ping.ts
+++ src/commands/General/ping.ts
@@ -1,19 +1,17 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command, CommandOptions } from '@sapphire/framework';
-import { send } from '@sapphire/plugin-editable-commands';
-import type { Message } from 'discord.js';
 
 @ApplyOptions<CommandOptions>({
-	description: 'ping pong'
+	name: 'ping',
+	description: 'Ping bot to see if it is alive',
+	chatInputCommand: {
+		register: true
+	}
 })
-export class UserCommand extends Command {
-	public async messageRun(message: Message) {
-		const msg = await send(message, 'Ping?');
+export class PingCommand extends Command {
+	public async chatInputRun(interaction: Command.ChatInputInteraction) {
+		const content = `Pong! Bot Latency ${Math.round(this.container.client.ws.ping)}ms.`;
 
-		const content = `Pong! Bot Latency ${Math.round(this.container.client.ws.ping)}ms. API Latency ${
-			(msg.editedTimestamp || msg.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)
-		}ms.`;
-
-		return send(message, content);
+		interaction.reply({ content: content, ephemeral: true })
 	}
 }
