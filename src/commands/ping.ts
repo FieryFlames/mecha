a609dc26caee2eae442dcb6f96768637514e8269
Jack Matthews <jm5112356@gmail.com>
2022-06-19 13:52:58 -0400

    Initial bot

---
+++ src/commands/ping.ts
@@ -0,0 +1,26 @@
+import { isMessageInstance } from '@sapphire/discord.js-utilities';
+import { Command } from '@sapphire/framework';
+
+export class PingCommand extends Command {
+	public constructor(context: Command.Context, options: Command.Options) {
+		super(context, {
+			...options,
+			name: 'ping',
+			description: 'Ping bot to see if it is alive',
+			chatInputCommand: {
+				register: true
+			}
+		});
+	}
+
+	public async chatInputRun(interaction: Command.ChatInputInteraction) {
+		const msg = await interaction.reply({ content: `Ping?`, ephemeral: true, fetchReply: true });
+		if (isMessageInstance(msg)) {
+			const diff = msg.createdTimestamp - interaction.createdTimestamp;
+			const ping = Math.round(this.container.client.ws.ping);
+			await interaction.editReply(`Pong 🏓! (Round trip took: ${diff}ms. Heartbeat: ${ping}ms.)`);
+			return;
+		}
+		await interaction.editReply('Failed to retrieve ping :(');
+	}
+}
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import './lib/setup';
+import { LogLevel, SapphireClient } from '@sapphire/framework';
+
+const client = new SapphireClient({
+	logger: {
+		level: LogLevel.Debug
+	},
+	shards: 'auto',
+	intents: [
+		'GUILDS',
+		'GUILD_MEMBERS',
+		'GUILD_BANS',
+		'GUILD_EMOJIS_AND_STICKERS',
+		'GUILD_VOICE_STATES',
+		'GUILD_MESSAGES',
+		'GUILD_MESSAGE_REACTIONS'
+	],
+	presence: {
+		status: 'online',
+		activities: [{ name: 'Fortnite', type: 0 }]
+	}
+});
+
+const main = async () => {
+	try {
+		client.logger.info('Logging in');
+		await client.login();
+	} catch (error) {
+		client.logger.fatal(error);
+		client.destroy();
+		process.exit(1);
+	}
+};
+
+main();
